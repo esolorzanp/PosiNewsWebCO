74c9ec838a5672f32813844393e49f7878d80195
esolorzanp <esolorzanop@estudiante.uniajc.edu.co>
2023-11-04 03:07:09 -0500

    ** Se finaliza front de Login. ** Se crea array object dataNews

---
+++ js/dataNews.js
@@ -0,0 +1,122 @@
+const dataPosiNews = [
+  {
+    id: 1,
+    editorial: "Semana Colombia",
+    titular:
+      "James Rodríguez, ante un posible gran cambio para 2024: atento a oferta desde Arabia",
+    subtitular:
+      "El volante colombiano tendría más opciones de jugar y además recuperaría la número ‘10′.",
+    seccion: "Deportes",
+    periodista: "Redacción Deportes",
+    fecha: "2 de noviembre de 2023",
+    img:
+      "https://www.semana.com/resizer/Outy7Vo-JsGnrxGcAa7Ddcj1Qu8=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/IAU3SDKELJGH3M3YRYVP3AT4ZU.jpg",
+    url:
+      "https://www.semana.com/deportes/articulo/james-rodriguez-ante-un-posible-gran-cambio-para-2024-atento-a-oferta-desde-arabia/202314/",
+  },
+  {
+    id: 2,
+    editorial: "Semana Colombia",
+    titular:
+      "Estos son los usos caseros que se le pueden dar a la cáscara de la cebolla",
+    subtitular:
+      "Estos residuos tienen propiedades que pueden aprovecharse de varias formas. Una de ellas, abonar el jardín.",
+    seccion: "Salud",
+    periodista: "Redacción Cómo",
+    fecha: "3 de noviembre de 2023",
+    img:
+      "https://www.semana.com/resizer/lYU6vWLYdOzfr3uXUdE9Bf_HpOk=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/L3OVGK7PLFB7HDWRYGIR3EGTY4.jpg",
+    url:
+      "https://www.semana.com/como/articulo/estos-son-los-usos-caseros-que-se-le-pueden-dar-a-la-cascara-de-la-cebolla/202313/",
+  },
+  {
+    id: 3,
+    editorial: "Semana Colombia",
+    titular:
+      "Primeros constructores de vivienda sin cuota inicial, entre los ganadores del premio Empresario del año 2023",
+    subtitular:
+      "La santandereana Constructora Marval obtuvo el máximo galardón en los Premios Empresario Colombiano 2023 de la Universidad del Rosario. Tres jóvenes líderes también sobresalieron por su empuje.",
+    seccion: "Premios",
+    periodista: "Redacción Economía",
+    fecha: "3 de noviembre de 2023",
+    img:
+      "https://www.semana.com/resizer/gFh23bc-mqOi7URJiwpkrQbCtbo=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/7MRH3MHN6ZFWPL3V3KS2IK747M.jpg",
+    url:
+      "https://www.semana.com/economia/empresas/articulo/primeros-constructores-de-vivienda-sin-cuota-inicial-entre-los-ganadores-del-premio-empresario-del-ano-2023/202300/",
+  },
+  {
+    id: 4,
+    editorial: "Semana Colombia",
+    titular:
+      "Producción de café tuvo una impresionante recuperación en octubre, según la Federación Nacional de Cafeteros",
+    subtitular:
+      "La buena noticia va de la mano con el incremento en las exportaciones del grano.",
+    seccion: "Agro",
+    periodista: "Redacción Economía",
+    fecha: "3 de noviembre de 2023",
+    img:
+      "https://www.semana.com/resizer/QqFUN2BD73TFdhhANWbSr49FIZg=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/GJNJYOINKZHCTBIAGUTQAU6JGU.jpg",
+    url:
+      "https://www.semana.com/economia/empresas/articulo/produccion-de-cafe-tuvo-una-impresionante-recuperacion-en-octubre-segun-la-federacion-nacional-de-cafeteros/202324/",
+  },
+  {
+    id: 5,
+    editorial: "Semana Colombia",
+    titular:
+      "En Canadá están buscando colombianos; ofrecen “pasaje de avión” y “ayuda para establecerse” en ese país",
+    subtitular:
+      "El proceso de selección final le compete única y exclusivamente al empresario.",
+    seccion: "Economía",
+    periodista: "Redacción Economía",
+    fecha: "3 de noviembre de 2023",
+    img:
+      "https://www.semana.com/resizer/eEvkUrtxeAu1mGwr_fM9jwSqWqo=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/YUKIGIGCCBBXDNSXZA455NVDRU.jpg",
+    url:
+      "https://www.semana.com/economia/articulo/en-canada-estan-buscando-colombianos-ofrecen-pasaje-de-avion-y-ayuda-para-establecerse-en-ese-pais/202327/",
+  },
+  {
+    id: 6,
+    editorial: "Semana Colombia",
+    titular:
+      "Max Verstappen dio cátedra en la ‘pole’ del GP de Brasil; así quedó la grilla de salida",
+    subtitular:
+      "El vigente campeón volvió a mostrar su poderío en el circuito de Interlagos.",
+    seccion: "Deportes",
+    periodista: "Redacción Deportes",
+    fecha: "3 de noviembre de 2023",
+    img:
+      "https://www.semana.com/resizer/H_QW1spYh9iWjnJrzhqvjWM_3yY=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/BR2HTOLUJBBODM55RWTRNX5JSQ.png",
+    url:
+      "https://www.semana.com/deportes/articulo/max-verstappen-dio-catedra-en-la-pole-del-gp-de-brasil-asi-quedo-la-grilla-de-salida/202329/",
+  },
+  {
+    id: 7,
+    editorial: "Semana Colombia",https://www.semana.com/resizer/nfyXwtYX5UtXU6FFgziYY6gaRXU=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/B4TGL52G3VCXZLVPWZFNP66MKI.jpg
+    titular:
+      "Esta es la estrategia que llevó las iniciativas sociales en Colombia a otro nivel",
+    subtitular:
+      "A través del programa Aflora, la Fundación Bolívar Davivienda trabaja por contribuir al empoderamiento de las organizaciones sociales desde lo económico y la autogestión. Anualmente se les reconoce el esfuerzo en un encuentro que este año contará con el reconocido emprendedor social Miquel de Paladella.",
+    seccion: "Desarrollo social",
+    periodista: "Redacción Semana",
+    fecha: "3 de noviembre de 2023",
+    img:
+      "https://www.semana.com/resizer/_s3fVO6M54GyTUBLK2k6ka0JYQw=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/U4NAXLHZGBEILJ3QV2MOG2KFX4.jpeg",
+    url:
+      "https://www.semana.com/mejor-colombia/articulo/esta-es-la-estrategia-que-llevo-las-iniciativas-sociales-en-colombia-a-otro-nivel/202300/",
+  },
+  {
+    id: 8,
+    editorial: "Semana Colombia",
+    titular:
+      "Dunkin’ Donuts: de emprendimiento familiar a una franquicia que emplea 1.200 jóvenes en Colombia",
+    subtitular:
+      "Hace 40 años, Miguel Merino, trajo a Colombia la franquicia estadounidense. Hoy, se ha consolidado como una marca apreciada por los colombianos y una de las primeras oportunidades de empleo para muchos jóvenes.",
+    seccion: "Empresas",
+    periodista: "Redacción Semana",
+    fecha: "3 de noviembre de 2023",
+    img:
+      "https://www.semana.com/resizer/_s3fVO6M54GyTUBLK2k6ka0JYQw=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/U4NAXLHZGBEILJ3QV2MOG2KFX4.jpeg",
+    url:
+      "https://www.semana.com/mejor-colombia/articulo/dunkin-donuts-de-emprendimiento-familiar-a-una-franquicia-que-emplea-1200-jovenes/202342/",
+  },
+];
