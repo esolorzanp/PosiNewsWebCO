05f28a4642bec32fbf0bad2c9d3baef1dd972b0d
esolorzanp <esolorzanop@estudiante.uniajc.edu.co>
2023-11-12 00:26:20 -0500

    * Se agrega manejo de eventos y listener a input tipo text para validacioines y a botones para llamado de métodos

--- js/login.js
+++ js/login.js
@@ -1,51 +1,61 @@
+const MIN_FIELD = 8;
+const MAX_FIELD = 99;
+const MIN_LCASE = 1;
+const MIN_UCASE = 1;
+const MIN_NUMBERS = 2;
+
 const users = [];
+const userInput = document.querySelector("#userInput");
+const passInput = document.querySelector("#passInput");
+const loginButton = document.querySelectorAll(".button");
+
+let fieldUserIsValidated = false;
+let fieldPassIsValidated = false;
+
+userInput.addEventListener("change", (e) => {
+  let x = e.target.value;
+  validateUserInput(x);
+});
+
+passInput.addEventListener("change", (e) => {
+  let x = e.target.value;
+  validatePassInput(x);
+});
+
+loginButton[0].addEventListener("click", () => {
+  onLogin();
+});
+
+loginButton[1].addEventListener("click", () => {
+  onClean();
+});
 
 const onClean = () => {
   document.getElementById("userInput").value = "";
   document.getElementById("passInput").value = "";
+  document.getElementById("login__message").innerHTML = "";
+  document.getElementById("userInput").value =
+    "esolorzanop@estudiantes.uniajc.edu.co";
+  document.getElementById("passInput").value = "Solo2023";
+  fieldUserIsValidated = true;
+  fieldPassIsValidated = true;
 };
 
 const onLogin = () => {
-  const MIN_FIELD = 8;
-  const MAX_FIELD = 99;
-  const MAX_LCASE = 1;
-  const MAX_UCASE = 1;
-  const MAX_NUMBERS = 3;
   let iUser = document.getElementById("userInput").value;
   let iPass = document.getElementById("passInput").value;
   initErrorMessageSection();
   showMessageSuccess("Validando usuario...");
+
   if (isEmpty(iUser)) {
-    showError(`El campo "Username" se encuentra vacio`);
-  } else if (iUser.length > MAX_FIELD) {
+    showError(`* El campo "Username" se encuentra vacio\n`);
+  }
+  if (isEmpty(iPass)) {
+    showError(`* El campo "Password" se encuentra vacio`);
+  }
+  if (!fieldUserIsValidated || !fieldPassIsValidated) {
     showError(
-      `El campo "Username" supera los ${MIN_FIELD} caracteres. (Número de caracteres ingresados: ${iUser.length})`
-    );
-  } else if (!containCharacters(iUser, 1, "@")) {
-    showError(
-      `El campo "Username" no corresponde a un correo electrónico o email`
-    );
-  } else if (isEmpty(iPass)) {
-    showError(`El campo "Password" se encuentra vacio`);
-  } else if (iPass.length < MIN_FIELD) {
-    showError(
-      `El campo "Password" debe tener al menos ${MIN_FIELD} caracteres. (Número de caracteres ingresados: ${iPass.length})`
-    );
-  } else if (iPass.length > MAX_FIELD) {
-    showError(
-      `El campo "Password" supera los ${MAX_FIELD} caracteres. (Número de caracteres ingresados: ${iPass.length})`
-    );
-  } else if (!containLowerCase(iPass, MAX_LCASE)) {
-    showError(
-      `El campo "Password" debe contener al menos ${MAX_LCASE} letra(s) minúscula(s).`
-    );
-  } else if (!containUpperCase(iPass, MAX_UCASE)) {
-    showError(
-      `El campo "Password" debe contener al menos ${MAX_UCASE} letra(s) mayúscula(s).`
-    );
-  } else if (!containNumbers(iPass, MAX_NUMBERS)) {
-    showError(
-      `El campo "Password" debe contener al menos ${MAX_NUMBERS} número(s).`
+      `* Los campos de "Username" y "Password" no cumplen los requerimientos mínimos.`
     );
   } else {
     existUser(iUser)
@@ -54,6 +64,9 @@ const onLogin = () => {
           validateUserPass(iUser, iPass)
             .then((resp) => {
               showMessageSuccess("Ingresando a la aplicación...");
+              redirectPage().then((resp) => {
+                window.location.href = resp;
+              });
             })
             .catch((error) => {
               showError(error);
@@ -66,12 +79,85 @@ const onLogin = () => {
   }
 };
 
+const redirectPage = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("pages/listNews.html");
+    }, 2500);
+  });
+};
+
+const validateUserInput = (x) => {
+  let b = true;
+  const emailRegEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  initErrorMessageSection();
+  if (x.length > MAX_FIELD) {
+    showError(
+      `* El campo "Username" supera los ${MAX_FIELD} caracteres. (Número de caracteres ingresados: ${x.length})`
+    );
+    b = false;
+  }
+  if (!emailRegEX.test(x)) {
+    showError(
+      `* El campo "Username" no corresponde a un correo electrónico o email`
+    );
+    b = false;
+  }
+  fieldUserIsValidated = b;
+};
+
+const validatePassInput = (x) => {
+  let b = true;
+  const lowerLettersRegEx = /[a-z]/g;
+  const upperLettersRegEx = /[A-Z]/g;
+  const numbersRegEx = /[0-9]/g;
+  const numLowerLetters = !lowerLettersRegEx.test(x)
+    ? 0
+    : x.match(lowerLettersRegEx).length;
+  const numUpperLetters = !upperLettersRegEx.test(x)
+    ? 0
+    : x.match(upperLettersRegEx).length;
+  const numNumbers = !numbersRegEx.test(x) ? 0 : x.match(numbersRegEx).length;
+  initErrorMessageSection();
+  if (x.length < MIN_FIELD) {
+    showError(
+      `* El campo "Password" debe tener al menos ${MIN_FIELD} caracteres. (Número de caracteres ingresados: ${x.length})`
+    );
+    b = false;
+  }
+  if (x.length > MAX_FIELD) {
+    showError(
+      `* El campo "Password" supera los ${MAX_FIELD} caracteres. (Número de caracteres ingresados: ${x.length})`
+    );
+    b = false;
+  }
+  if (numLowerLetters < MIN_LCASE) {
+    showError(
+      `* El campo "Password" debe contener al menos ${MIN_LCASE} letra(s) minúscula(s).`
+    );
+    b = false;
+  }
+  if (numUpperLetters < MIN_UCASE) {
+    showError(
+      `* El campo "Password" debe contener al menos ${MIN_UCASE} letra(s) mayúscula(s).`
+    );
+    b = false;
+  }
+  if (numNumbers < MIN_NUMBERS) {
+    showError(
+      `* El campo "Password" debe contener al menos ${MIN_NUMBERS} número(s).`
+    );
+    b = false;
+  }
+  fieldPassIsValidated = b;
+};
+
 const initErrorMessageSection = () => {
   document.getElementById("login__message").innerHTML = "";
 };
 
 const showError = (x) => {
-  initErrorMessageSection();
+  //initErrorMessageSection();
   let pError = document.createElement("p");
   pError.className = "login__message--error";
   pError.innerHTML = x;
@@ -88,30 +174,4 @@ const showMessageSuccess = (x) => {
 
 const isEmpty = (x) => {
   return x === "";
-};
-
-const containLowerCase = (x, y) => {
-  const carValidos = "abcdefghijklmnopqrstuvwxyz";
-  return containCharacters(x, y, carValidos);
-};
-
-const containUpperCase = (x, y) => {
-  const carValidos = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  return containCharacters(x, y, carValidos);
-};
-
-const containNumbers = (x, y) => {
-  const carValidos = "1234567890";
-  return containCharacters(x, y, carValidos);
-};
-
-const containCharacters = (x, y, carValidos) => {
-  let j = 0;
-  for (let i in x) {
-    if (carValidos.includes(x[i])) {
-      j++;
-      if (j === y) return true;
-    }
-  }
-  return false;
 };
