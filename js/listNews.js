4c5bd16fa69e1ebcb47488e54a845349d056cbb0
esolorzanp <esolorzanop@estudiante.uniajc.edu.co>
2023-11-17 01:20:39 -0500

    ** se incluye listado de noticias

--- js/listNews.js
+++ js/listNews.js
@@ -19,7 +19,8 @@ btnSearch.addEventListener("click", () => {
   filtersNews.editorial = inputEditorial.value;
   getAllNews(filtersNews).then((resp) => {
     console.log("respuesta promise getAllNews");
-    console.log(resp);
+    //  console.log(resp);
+    renderTable(resp.results);
   });
 });
 
@@ -29,3 +30,74 @@ btnCleanFilters.addEventListener("click", () => {
   inputEditorial.innerHTML = "";
   sectionListNewsTable.innerHTML = "";
 });
+
+const renderTable = (data) => {
+  const sectionTable = document.getElementById("list-news__news-table");
+
+  console.log(data);
+
+  let tTable = document.createElement("table");
+  tTable.id = "table";
+  tTable.className = "table";
+  sectionTable.innerHTML = "";
+  sectionTable.appendChild(tTable);
+
+  let thRow = document.createElement("tr");
+  thRow.id = "thRow";
+  document.getElementById(tTable.id).appendChild(thRow);
+
+  let thCol1 = document.createElement("th");
+  thCol1.innerHTML = "Fecha";
+  document.getElementById(thRow.id).appendChild(thCol1);
+  let thCol2 = document.createElement("th");
+  thCol2.innerHTML = "Titular";
+  document.getElementById(thRow.id).appendChild(thCol2);
+  let thCol3 = document.createElement("th");
+  thCol3.innerHTML = "Fuente";
+  document.getElementById(thRow.id).appendChild(thCol3);
+  let thCol4 = document.createElement("th");
+  thCol4.innerHTML = "Categoría";
+  document.getElementById(thRow.id).appendChild(thCol4);
+  let thCol5 = document.createElement("th");
+  thCol5.innerHTML = "Ciudad";
+  document.getElementById(thRow.id).appendChild(thCol5);
+  let thCol6 = document.createElement("th");
+  thCol6.innerHTML = "";
+  //thCol6.style.width = "200px";
+  document.getElementById(thRow.id).appendChild(thCol6);
+
+  let i = 0;
+  data.map((e) => {
+    let trRow = document.createElement("tr");
+    trRow.id = `trRow${i}`;
+    document.getElementById(tTable.id).appendChild(trRow);
+
+    let tdCol1 = document.createElement("td");
+    tdCol1.innerHTML = e.fecha;
+    document.getElementById(trRow.id).appendChild(tdCol1);
+    let tdCol2 = document.createElement("td");
+    tdCol2.innerHTML = e.titular;
+    document.getElementById(trRow.id).appendChild(tdCol2);
+    let tdCol3 = document.createElement("td");
+    tdCol3.innerHTML = e.fuente;
+    document.getElementById(trRow.id).appendChild(tdCol3);
+    let tdCol4 = document.createElement("td");
+    tdCol4.innerHTML = e.categoria;
+    document.getElementById(trRow.id).appendChild(tdCol4);
+    let tdCol5 = document.createElement("td");
+    tdCol5.innerHTML = e.ciudad;
+    document.getElementById(trRow.id).appendChild(tdCol5);
+    let tdCol6 = document.createElement("td");
+    tdCol6.id = `tdCol6${i}`;
+    document.getElementById(trRow.id).appendChild(tdCol6);
+    let btnVerMas = document.createElement("button");
+    btnVerMas.innerText = "View more";
+    btnVerMas.className = "button button--success";
+    document.getElementById(tdCol6.id).appendChild(btnVerMas);
+    i++;
+  });
+
+  /*
+  const trRow = document.createElement("tr");
+  tTable.appendChild(tTable);*/
+};
--- js/newsApi.js
+++ js/newsApi.js
@@ -23,7 +23,7 @@ const queryNewsByFilters = (filtersNews) => {
   const newsArray = [];
   const dataNewsArray = {
     info: {
-      cantReg: 0,
+      //cantReg: 0,
       totPages: 0,
       totRecordsQueried: 0,
       page: 0,
@@ -36,65 +36,19 @@ const queryNewsByFilters = (filtersNews) => {
     results: [],
   };
 
-  for (let e in dataBase.news) {
+  dataBase.news.map((e) => {
     newsArray.push(e);
-  }
+  });
+
   for (let i = 0; i < CANT_X_PAGE; i++) {
     let x = dataNewsArray.info.page * CANT_X_PAGE + i;
     dataNewsArray.results.push(newsArray[x]);
   }
-  dataNewsArray.info.cantReg = dataNewsArray.results.length;
+  //dataNewsArray.info.cantReg = newsArray.length //dataNewsArray.results.length;
   dataNewsArray.info.totRecordsQueried = newsArray.length;
   dataNewsArray.info.totPages =
     Math.floor(dataNewsArray.info.totRecordsQueried / CANT_X_PAGE) +
     (dataNewsArray.info.totRecordsQueried % CANT_X_PAGE > 0 ? 1 : 0);
-  console.log(dataNewsArray.info.cantReg);
-  //dataNewsArray.info.cantReg = dataNewsArray.results.length;
-  //dataNewsArray.info.totPages;
 
-  console.log(dataNewsArray);
-  /*
-  dataBase.news.map((n) => {
-    newsArray.push(n);
-  });
-  let cantReg = newsArray.length;
-  let totPages =
-    Math.floor(cantReg / CANT_X_PAGE) + (cantReg % CANT_X_PAGE > 0 ? 1 : 0);
-  dataNewsArray.cantReg = cantReg;
-  dataNewsArray.totPages = totPages;
-  dataNewsArray.page = [];
-
-  for (let i = 0; i < totPages; i++) {
-    const news = [];
-    const noti = {};
-    noti.new = {};
-    for (let j = 0; j < CANT_X_PAGE; j++) {
-      let pg = `page$[i]`;
-      let x = i * CANT_X_PAGE + j;
-      if (x === cantReg) {
-        break;
-      } else {
-        newsArray[x].map((k) => {
-          console.log(k);
-        });
-        noti.new = newsArray[x];
-        //news.new.push(newsArray[x]);
-      }
-    }
-    console.log(noti);
-    dataNewsArray.page.push(noti);
-  }
-  */
   return dataNewsArray;
 };
-
-/**
- * Cantidad por página = 10
- * Total elementos = 50
- * nro paginas = 50 / 10 = 5 + (50 % 10 > 0 ? 1 : 0)
- * filters: {category: "", editorial:"", country:""}
- *
- * Total elementos = 12
- * nro paginas = 12 / 10 = 1 + (12 % 10 > 0 ? 1 : 0)
- *
- */
